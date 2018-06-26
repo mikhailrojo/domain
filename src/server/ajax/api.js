23ce58fa9d39341c9623dbd218a5a563ad48dcaa
mmstepanov <mmstepanov@yamoney.ru>
2018-06-26 10:02:24 +0300

    Добавил пробрасывание урл без параметров

--- src/server/ajax/api.js
+++ src/server/ajax/api.js
@@ -1,5 +1,6 @@
 "use strict";
 const whois = require('../helper');
+const url = require('url');
 
 module.exports = async (req, res) => {
 	const {domain} = req.query;
@@ -7,12 +8,11 @@ module.exports = async (req, res) => {
 
 	console.log(`Domain to search: ${domain}`);
 
-
 	if (!domain || !isValidUrl) {
 		return res.send({success: false, error: 'Домен не обнаружен'});
 	}
 	try {
-		const result = await whois(domain);
+		const result = await whois(getPathName(domain));
 
 		if(!result) {
 			return res.send({success: true, result: 'Домен не был найден'})
@@ -27,10 +27,15 @@ module.exports = async (req, res) => {
 
 function isURL(str) {
 	const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
 		'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-		'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+		'(\\:\\d+)?'+ // port
+		'(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
+		'(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
 		'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 	return pattern.test(str);
+}
+
+function getPathName(str) {
+	return url.parse(str).pathname;
 }
